2fce83079fbe14272a0ffcc5cc61b3d433c59ba1
Connor Whitworth <Connor.Whitworth@softwire.com>
2023-07-04 11:16:34 +0100

    User specifies ruleset

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,29 +7,29 @@ function getTarget() {
     return target;
 }
 
-function fizzbuzz() {
+function fizzbuzz(target,rules) {
 
     for (let i = 1; i < (target+1); i++) {
         let toLog = [];
 
         // Multiple of 3: Fizz
-        if (i % 3 == 0) {
+        if (i % 3 == 0 && rules.includes(3)) {
             toLog.push("Fizz");
         }
 
         // Multiple of 13: Fezz (Before any word beginning 'B')
-        if (i % 13 == 0) {
+        if (i % 13 == 0 && rules.includes(13)) {
             toLog.push("Fezz");
         }
 
         // Multiple of 5: Buzz
-        if (i % 5 == 0) {
+        if (i % 5 == 0 && rules.includes(5)) {
             toLog.push("Buzz");
 
         }
 
         // Multiple of 7: Bang
-        if (i % 7 == 0) {
+        if (i % 7 == 0 && rules.includes(7)) {
             toLog.push("Bang");
         }
 
@@ -39,9 +39,9 @@ function fizzbuzz() {
         }
 
         // Multiple of 11: Bong
-        if (i % 11 == 0) {
+        if (i % 11 == 0 && rules.includes(11)) {
             // Multiple of 13 AND 11: FezzBong
-            if (i % 143 == 0) {
+            if (i % 143 == 0 && rules.includes(11) && rules.includes(13)) {
                 toLog = ["Fezz","Bong"];
             } else {
                 toLog = ["Bong"];
@@ -49,7 +49,7 @@ function fizzbuzz() {
         }
 
         // Multiple of 17: Reverse
-        if (i % 17 == 0) {
+        if (i % 17 == 0 && rules.includes(17)) {
             toLog = toLog.reverse();
         }
         
@@ -61,7 +61,11 @@ function fizzbuzz() {
 
 }
 
+let rules = [3,5,7,11,13,17];
+if (process.argv.length > 2) {
+    rules = process.argv[2].split(',').map(x=>+x);
+}
 let target = getTarget();
 // Now, we run the main function:
-fizzbuzz(target);
+fizzbuzz(target, rules);
 
